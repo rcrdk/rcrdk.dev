152c7ce980736c2c37f44f4917d16318e772501c
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2025-03-20 22:17:08 -0300

    refactor: projects and skills

---
+++ src/app/[locale]/components/skill-item.tsx
@@ -0,0 +1,7 @@
+type Props = {
+	name: string
+}
+
+export function SkillItem({ name }: Props) {
+	return <p>{name}</p>
+}
--- src/app/[locale]/components/skills.tsx
+++ src/app/[locale]/components/skills.tsx
@@ -1,64 +1,67 @@
 'use client'
 
-import { useEffect, useRef, useState } from 'react'
+import { useMemo, useState } from 'react'
 import { useTranslations } from 'next-intl'
 
+import { SkillItem } from '@/app/[locale]/components/skill-item'
 import AnimatedContent from '@/components/animated/animated-content'
 import { Section } from '@/components/ui/section'
+import { SkillCategories, SKILLS } from '@/data/skills'
+import { cn } from '@/utils/tailwind-cn'
 
 export function Skills() {
-	const [canDelayAnimations, setCanDelayAnimations] = useState(false)
-	const ref = useRef<HTMLDivElement>(null)
-
 	const __ = useTranslations('Skills')
-	const list = __.raw('list') as Record<string, string>[]
+	const categories = __.raw('categories') as { id: SkillCategories; title: string }[]
 
-	useEffect(() => {
-		const element = ref.current
-		if (!element) return
+	const [selectedCategory, setSelectedCategory] = useState<SkillCategories>('front-end')
 
-		const observer = new IntersectionObserver(
-			([entry]) => {
-				if (entry.isIntersecting) {
-					setCanDelayAnimations(true)
-					observer.unobserve(element)
-				}
-			},
-			{ threshold: 1 },
-		)
+	const skills = useMemo(() => {
+		return SKILLS.filter((item) => item.categories.includes(selectedCategory))
+	}, [selectedCategory])
 
-		observer.observe(element)
-
-		return () => observer.disconnect()
-	}, [])
+	function handleChangeCategory(category: SkillCategories) {
+		setSelectedCategory(category)
+	}
 
 	return (
 		<Section classNameCenter="max-xs:pt-9" className="layout:border-t-0">
-			<div ref={ref}>
-				<AnimatedContent distance={125} config={{ tension: 60, friction: 15 }} rootMargin="0px 0px 125px">
-					<h2 className="layout:mb-8 font-heading layout:text-6xl mb-8 block items-center text-3xl font-black tracking-tight text-balance sm:mb-12 sm:text-4xl md:max-w-full lg:static lg:block lg:h-auto lg:pr-0 dark:text-white">
-						{__('title')}
-					</h2>
-				</AnimatedContent>
+			<AnimatedContent distance={125} config={{ tension: 60, friction: 15 }} rootMargin="0px 0px 125px">
+				<h2 className="layout:mb-8 font-heading layout:text-6xl mb-8 block items-center text-3xl font-black tracking-tight text-balance sm:mb-12 sm:text-4xl md:max-w-full lg:static lg:block lg:h-auto lg:pr-0 dark:text-white">
+					{__('title')}
+				</h2>
+			</AnimatedContent>
 
-				<div className="flex flex-col gap-4 sm:gap-2">
-					{list.map((item, index) => (
-						<AnimatedContent
-							distance={125}
-							rootMargin="0px 0px 125px"
-							config={{ tension: 60, friction: 15 }}
-							key={item.title}
-							delay={canDelayAnimations ? (index + 1) * 50 : 0}
+			<div className="flex flex-wrap gap-2">
+				{categories.map((item) => (
+					<AnimatedContent
+						distance={125}
+						rootMargin="0px 0px 125px"
+						config={{ tension: 60, friction: 15 }}
+						key={item.id}
+					>
+						<button
+							className={cn(
+								'rounded-2xl border px-4 py-2 text-xs leading-none font-bold tracking-tight whitespace-nowrap uppercase transition-all select-none',
+								selectedCategory === item.id &&
+									'bg-content-light pointer-events-none border-transparent text-white dark:bg-white dark:text-black',
+								selectedCategory !== item.id &&
+									'focus-visible:border-accent-blue focus-visible:bg-accent-blue/10 focus-visible:text-accent-blue focus-visible:ring-accent-blue/40 cursor-pointer border-black/20 outline-none hover:border-black focus-visible:ring-4 active:scale-85 active:duration-150 dark:border-white/15 dark:hover:border-white dark:hover:text-white',
+							)}
+							tabIndex={selectedCategory === item.id ? -1 : 0}
+							onClick={() => handleChangeCategory(item.id)}
 						>
-							<p className="text-content-light/65 dark:text-content-dark/85 sm:text-md leading-[1.8] text-pretty md:text-lg">
-								<strong className="text-content-light block font-semibold sm:inline dark:text-white">
-									{item.title}:
-								</strong>{' '}
-								{item.text}.
-							</p>
-						</AnimatedContent>
-					))}
-				</div>
+							{item.title}
+						</button>
+					</AnimatedContent>
+				))}
+			</div>
+
+			<hr className="my-8 border-t border-black/10 dark:border-white/15 sm:dark:border-white/20" />
+
+			<div className="flex flex-wrap gap-2">
+				{skills.map((item) => (
+					<SkillItem name={item.name} key={item.name} />
+				))}
 			</div>
 		</Section>
 	)
