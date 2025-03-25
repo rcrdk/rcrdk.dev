61907b65b7610d4975aa52eb380274260f726ed2
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2025-03-25 13:50:53 -0300

    fix: game toast dismiss button

--- src/components/game/game-toast-content.tsx
+++ src/components/game/game-toast-content.tsx
@@ -1,3 +1,7 @@
+import { useCallback } from 'react'
+import { toast } from 'sonner'
+
+import { Button } from '@/components/ui/button'
 import { GameTaskUser } from '@/context/game-context'
 
 type Props = {
@@ -5,11 +9,15 @@ type Props = {
 }
 
 export function GameToastContent({ task }: Props) {
+	const handleDismissToast = useCallback(() => {
+		toast.dismiss(task.id)
+	}, [task.id])
+
 	return (
 		<div className="xs:gap-3 flex items-center gap-2">
 			<span className="size-auto shrink-0 text-3xl">{task.icon}</span>
 
-			<p className="flex flex-col gap-0.5">
+			<p className="flex grow flex-col gap-0.5">
 				<strong className="text-content-light max-xs:text-center flex grow items-center gap-2 text-sm leading-[1.25] text-balance dark:text-white">
 					{task.toastItem.title}
 
@@ -20,6 +28,10 @@ export function GameToastContent({ task }: Props) {
 
 				<span className="text-content-light dark:text-content-dark">{task.toastItem.hint}</span>
 			</p>
+
+			<Button size="xs" onClick={() => handleDismissToast()}>
+				Dispensar
+			</Button>
 		</div>
 	)
 }
