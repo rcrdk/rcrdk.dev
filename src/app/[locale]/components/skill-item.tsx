51df81771f188ec07368029c2a02136dc4921d06
Ricardo Augusto Kowalski <ricardoakowalski@gmail.com>
2025-03-26 09:49:31 -0300

    fix: better styles for skills on small devices

--- src/app/[locale]/components/skill-item.tsx
+++ src/app/[locale]/components/skill-item.tsx
@@ -11,7 +11,7 @@ export function SkillItem({ skill }: Props) {
 	const locale = useLocale()
 
 	return (
-		<p className="xs:text-base xs:py-1 xs:gap-2 flex items-center gap-1 rounded-xl border border-black/15 bg-black/3 px-2 py-2 text-sm text-black select-none dark:border-white/15 dark:bg-white/10 dark:text-white">
+		<p className="xs:text-sm xs:py-1 xs:gap-2 flex items-center gap-1 rounded-xl border border-black/15 px-2 py-1 text-xs text-black select-none sm:text-base dark:border-white/15 dark:text-white">
 			{skill.icon && (
 				<span className="xs:size-5 xs:[&_svg]:size-5 block size-4 [&_svg]:size-4 [&_svg]:fill-current">
 					{skill.icon}
